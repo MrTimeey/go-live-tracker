45f579ed785d192c692873be3120191b1da3bd0f
Kruse Tim <t.kruse@reply.de>
2024-03-29 10:35:22 +0100

    init

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	templateFunction := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+		tmpl.Execute(w, nil)
+	}
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/*", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", templateFunction)
+
+	fmt.Println("Server up and running at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
